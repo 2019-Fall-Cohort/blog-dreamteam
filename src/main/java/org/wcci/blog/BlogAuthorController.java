96b546b2ebaa0bf90e477dd57cb5d0209e60c46e
Brian Bross <brian.bross@gmail.com>
2019-10-20 16:57:34 -0400

    hunting bugs

--- src/main/java/org/wcci/blog/BlogAuthorController.java
+++ src/main/java/org/wcci/blog/BlogAuthorController.java
@@ -1,43 +1,43 @@
-package org.wcci.blog;
-
-import javax.annotation.Resource;
-
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-
-@Controller
-@RequestMapping("/author")
-public class BlogAuthorController {
-
-	@Resource
-	BlogAuthorStorage authors;
-	
-	@RequestMapping("")
-	public String getAuhtors(Model model) {
-		model.addAttribute("authors", authors.findAllTheBlogAuthors());
-		
-		return "posts";
-		
-	}
-	
-	@RequestMapping("/{id}")
-	public String getAuthor(@PathVariable ("id") long id, Model model) {
-		model.addAttribute("author", authors.findBlogAuthor(id));
-		
-		return "add_post";
-		
-	}
-	
-	@PostMapping("/add")
-	public String addAuthor(BlogAuthor authorName, Long authorId) {
-		BlogAuthor author = authors.findBlogAuthor(authorId);
-		authors.addBlogAuthor(authorName);
-		
-		return "redirect:add_post";
-	}
-	
-	
-}
+//package org.wcci.blog;
+//
+//import javax.annotation.Resource;
+//
+//import org.springframework.stereotype.Controller;
+//import org.springframework.ui.Model;
+//import org.springframework.web.bind.annotation.PathVariable;
+//import org.springframework.web.bind.annotation.PostMapping;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//
+//@Controller
+//@RequestMapping("/author")
+//public class BlogAuthorController {
+//
+//	@Resource
+//	BlogAuthorStorage authors;
+//	
+//	@RequestMapping("")
+//	public String getAuhtors(Model model) {
+//		model.addAttribute("authors", authors.findAllTheBlogAuthors());
+//		
+//		return "posts";
+//		
+//	}
+//	
+//	@RequestMapping("/{id}")
+//	public String getAuthor(@PathVariable ("id") long id, Model model) {
+//		model.addAttribute("author", authors.findBlogAuthor(id));
+//		
+//		return "add_post";
+//		
+//	}
+//	
+//	@PostMapping("/add")
+//	public String addAuthor(BlogAuthor authorName, Long authorId) {
+//		BlogAuthor author = authors.findBlogAuthor(authorId);
+//		authors.addBlogAuthor(authorName);
+//		
+//		return "redirect:add_post";
+//	}
+//	
+//	
+//}
