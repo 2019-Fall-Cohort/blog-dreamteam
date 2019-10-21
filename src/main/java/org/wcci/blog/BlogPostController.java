2e8645fe3e462ec63680b5b68bc7dc1e0a5ddb39
Brian Bross <brian.bross@gmail.com>
2019-10-21 02:04:41 -0400

    adjusted controllers

--- src/main/java/org/wcci/blog/BlogPostController.java
+++ src/main/java/org/wcci/blog/BlogPostController.java
@@ -1,44 +1,71 @@
-//package org.wcci.blog;
-//
-//import java.time.LocalDateTime;
-//
-//import javax.annotation.Resource;
-//
-//import org.springframework.stereotype.Controller;
-//import org.springframework.ui.Model;
-//import org.springframework.web.bind.annotation.PathVariable;
-//import org.springframework.web.bind.annotation.PostMapping;
-//import org.springframework.web.bind.annotation.RequestMapping;
-//
-//@Controller
-//@RequestMapping("/post")
-//public class BlogPostController {
-//
-//	@Resource
-//	BlogPostStorage posts;
-//
-//	@RequestMapping("")
-//	public String getPosts(Model model) {
-//		model.addAttribute("posts", posts.findAllTheBlogPosts());
-//
-//		return "posts";
-//
-//	}
-//
-//	@RequestMapping("/{id}")
-//	public String getPost(@PathVariable("id") long id, Model model) {
-//		model.addAttribute("post", posts.findBlogPost(id));
-//
-//		return "post";
-//
-//	}
-//
-//	@PostMapping("/add")
-//	public String addPost(String blogPostTitle, BlogAuthor blogAuthor, LocalDateTime blogDateTime, BlogGenre blogGenre,
-//		   String blogBody, Long postId, BlogTag... blogTags) {
-//		BlogPost post = posts.findBlogPost(postId);
-//		posts.add(new BlogPost (blogPostTitle, blogAuthor, blogDateTime, blogGenre, blogBody, blogTags));
-//
-//		return "redirect:/post/" + postId;
-//	}
-//}
+package org.wcci.blog;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping
+public class BlogPostController {
+
+	@Autowired
+	BlogAuthorStorage authorStorage;
+	@Autowired
+	BlogGenreStorage genreStorage;
+	@Autowired
+	BlogPostStorage postStorage;
+	@Autowired
+	BlogTagStorage tagStorage;
+	
+	@GetMapping("all_posts")
+	public String getAllBlogPosts(Model model) {
+		model.addAttribute("posts", postStorage.findAllTheBlogPosts());
+		return "posts";
+	}
+	
+	@GetMapping("/add_post")
+	public String getAddReview(Model model) {
+		model.addAttribute("authors", authorStorage.findAllTheBlogAuthors());
+		model.addAttribute("genres", genreStorage.findAllTheBlogGenres());
+		model.addAttribute("tags", tagStorage.findAllTheBlogTags());
+		return "add_post";
+	}
+	
+	@PostMapping("/add")
+	public String addPost(String blogPostTitle, Long blogAuthorId, Long blogGenreId, String blogBody, Long... blogTagList) {
+		BlogAuthor author = authorStorage.findBlogAuthor(blogAuthorId);
+		LocalDateTime dateTime = LocalDateTime.now();
+		BlogGenre genre = genreStorage.findBlogGenre(blogGenreId);
+		List<BlogTag> tags = new ArrayList<BlogTag>();
+		Long blogPostId;
+		if (blogTagList != null ) {
+			for (Long id : blogTagList) {
+				tags.add(tagStorage.findBlogTag(id));
+			}
+			BlogPost postToAdd = new BlogPost(blogPostTitle, author, dateTime, genre, blogBody, tags);
+			postStorage.add(postToAdd);
+			blogPostId = postToAdd.getId();
+		} else {
+			BlogPost postToAdd = new BlogPost(blogPostTitle, author, dateTime, genre, blogBody);
+			postStorage.add(postToAdd);
+			blogPostId = postToAdd.getId();
+		}
+		return "redirect:all_posts";
+	}
+	
+	@GetMapping("/posts/{id}")
+	public String getPost(@PathVariable Long id, Model model) {
+		BlogPost post = postStorage.findBlogPost(id);
+		model.addAttribute("post", post);
+		return "post";
+
+	}
+}
